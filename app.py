cfd8d460701b6ed49eaccb8cf6fa79a6d0b59d75
Kelwin Savoia <kelwinssavoia@gmail.com>
2021-06-01 19:36:24 -0300

    Add Bugs

--- app.py
+++ app.py
@@ -9,5 +9,15 @@ csfr = CSRFProtect(app)
 def pagina_inicial():
     return "Hello World"
 
+@app.route("/bug")
+def bad():
+    try:
+        raise TypeError()
+    except TypeError as e:
+        print(e)
+    except TypeError as e:
+        print("Duplicado.")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
